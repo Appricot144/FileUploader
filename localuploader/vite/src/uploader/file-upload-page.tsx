af0618c86ccc267a81928b9d1ccd87340a3f892d
Appricot144 <aniline.96.256@gmail.com>
2025-05-04 14:08:11 +0900

    fix: upload file

--- localuploader/vite/src/uploader/file-upload-page.tsx
+++ localuploader/vite/src/uploader/file-upload-page.tsx
@@ -5,15 +5,8 @@ import type React from "react";
 import { JSX, useState } from "react";
 import Layout from "../layout";
 import { UploadSimple, X } from "@phosphor-icons/react";
-// import useSWR from "swr";
-
-// function Upload(distination: string, file: File) {
-//   const { data, error, isLoading } = useSWR("/api/upload", fetcher);
-
-//   return <></>;
-//}
-
-// const fetcher = (...args) => fetch(...args).then((res) => res.json());
+import useSWRMutation from "swr";
+import toast from 'react-hot-toast';
 
 export default function FileUploadPage() {
   const [destination, setDestination] = useState("");
@@ -23,6 +16,8 @@ export default function FileUploadPage() {
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       setFiles(e.target.files);
+    } else {
+      setFiles(files);
     }
   };
 
@@ -44,12 +39,39 @@ export default function FileUploadPage() {
     }
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  // いい感じに分離したい
+  async function uploadFiles(url: string, files: FileList) {
+    const formData = new FormData();
+    
+    // fix me
+    formData.append("destination", destination);
+    for (let i = 0; i < files?.length; i++) {
+      const f = files.item(i);
+      if (f) {
+        formData.append(f.name, new Blob([f], { type: f.type}));
+      }
+    }
+
+    return await fetch(url, {
+      method: 'POST',
+      body: formData,
+    }).then(res => res.json())
+  }
+  const { trigger, isMutating } = useSWRMutation("/api/upload", uploadFiles);
+  
+  async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-
+    
     // file upload
+    try {
+      await trigger(files);
+    } catch (e) {
+      toast.error(`Faild upload files!! : ${e}`);
+    }
 
-    alert(`File "${files && Array.from(files).toString()}" would be uploaded to "${destination}"`); // delete
+    // fix me
+    if (isMutating) toast.loading("Waiting...");
+    toast.success("Successfully uploaded!");
   };
 
   return (
@@ -97,24 +119,24 @@ export default function FileUploadPage() {
                   className="hidden"
                   multiple
                   onChange={handleFileChange}
-                />
+                  />
                 <div className="flex flex-col items-center justify-center gap-2">
                   <UploadSimple className="h-10 w-10 text-gray-400" />
                   <label
                     htmlFor="file"
                     className="cursor-pointer text-blue-600 hover:underline"
-                  >
+                    >
                     ファイルを選択
                   </label>
                   <p className="text-sm text-gray-500">
-                    または、ここにファイルをドラッグ＆ドロップ“
+                    または、ここにファイルをドラッグ＆ドロップ
                   </p>
                 </div>
               </div>
             </div>
             <button
               type="submit"
-              disabled={files?.length == 0 || !destination}
+              disabled={!files || !destination || files.length == 0}
               className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
               >
               アップロード
@@ -133,7 +155,7 @@ type UpFile = {
   rawFile: File | null;
 }
 
-function UploadFile({ file, deleteFile }: { file: UpFile, deleteFile: (id: number) => void }) {
+function UploadFile({ file, onDeleteFile }: { file: UpFile, onDeleteFile: (id: number) => void }) {
   return (
     <div className="flex justify-between items-center mt-1 p-2 bg-gray-100 rounded-md">
       <div>
@@ -142,7 +164,7 @@ function UploadFile({ file, deleteFile }: { file: UpFile, deleteFile: (id: numbe
             {((file.rawFile ? file.rawFile.size : 1) / 1024).toFixed(2)} KB
         </p>
       </div>
-      <X onClick={() => { deleteFile(file.id) }} />
+      <X onClick={() => { onDeleteFile(file.id) }} />
     </div>
   );
 }
@@ -156,16 +178,14 @@ function UploadFileList(
     setFiles: React.Dispatch<React.SetStateAction<FileList | null>>,
   }
 ) {
-  if (!fileList) {
-    return <></>;
-  }
-
   let upFileList: UpFile[] = [];
-  for (let i = 0; i < fileList?.length; i++) {
-    upFileList.push({
-      id: i,
-      rawFile: fileList?.item(i),
-    });
+  if (fileList) {
+    for (let i = 0; i < fileList?.length; i++) {
+      upFileList.push({
+        id: i,
+        rawFile: fileList?.item(i),
+      });
+    }
   }
 
   // event handler
@@ -182,8 +202,8 @@ function UploadFileList(
   }
 
   const rows: JSX.Element[] = [];
-  Array.from(fileList).forEach((file: File, i: number) => {
-    rows.push(<UploadFile file={{ id: i, rawFile: file }} deleteFile={deleteFile} />);
+  upFileList.forEach((file: UpFile) => {
+    rows.push(<UploadFile file={file} onDeleteFile={deleteFile} />);
   });
   return (
     <div className="space-y-2">
