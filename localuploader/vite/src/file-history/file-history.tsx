d7ef3159051404ada46d1bbc40243009c12ed599
Appricot144 <hub256144@gmail.com>
2025-05-26 01:51:16 +0900

    wip: design fix file history

--- localuploader/vite/src/file-history/file-history.tsx
+++ localuploader/vite/src/file-history/file-history.tsx
@@ -34,31 +34,47 @@ export default function FileHistory() {
   return (
     <div className="bg-background flex-1 p-6">
       <div className="flex item-center flex-col gap-2 mb-6 max-w-2xl mx-auto">
-        <h1 className="text-2xl font-bold mb-6">File History</h1>
-        <ol className="relative border-s-2 border-gray-200 dark:border-gray-700">
-          <li className="mb-10 ms-6">
-            <div className="flex flex-col gap-2">
-              <span className="absolute flex items-center justify-center w-6 h-6 bg-gray-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                <GitCommit />
-              </span>
-              <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                Flowbite Application UI v2.0.0{" "}
-                <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300 ms-3">
-                  Latest
-                </span>
-              </h3>
+        {/* File History */}
+        <div className="mb-6">
+          <h1 className="text-2xl font-bold mb-2">File History</h1>
+          <p className="text-sm text-gray-500">
+            Here are the files you have succecally uploaded.
+          </p>
+        </div>
+        <ol className="relative border-s-2 border-gray-200 dark:border-gray-700 mb-10">
+          <li className="flex flex-col gap-2 ps-6">
+            <span className="absolute flex items-center justify-center w-6 h-6 bg-white rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-gray-900">
+              <GitCommit />
+            </span>
+            <div>
+              <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                Released on January 13th, 2022
+                <Latest />
+              </time>
             </div>
-            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              Released on January 13th, 2022
-            </time>
-            <ul className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-              <li>file1.txt</li>
-              <li>file2.txt</li>
-              <li>file3.txt</li>
-            </ul>
+            <div className="flex flex-row grow mb-1">
+              <div className="mb-4">
+                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  Flowbite Application UI v2.0.0
+                </h3>
+                <ul className="text-base font-normal text-gray-500 dark:text-gray-400">
+                  <li>file1.txt</li>
+                  <li>file2.txt</li>
+                  <li>file3.txt</li>
+                </ul>
+              </div>
+            </div>
           </li>
         </ol>
       </div>
     </div>
+  );
+}
+
+function Latest() {
+  return (
+    <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300 ms-3">
+      Latest
+    </span>
   );
 }
